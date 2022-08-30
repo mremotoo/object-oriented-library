414711b49dab587f929d7da88e186b40b5faa998
mremoto <mremotoo@gmail.com>
2022-08-30 09:43:22 +0800

    Add button which brings up forms. Remove evenetlistener afterward.

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-let myLibrary = ['harry', 'potter'];
+let myLibrary = [];
 //constructor
 function Book() {
     
 }
 
 function addBookToLibrary() {
-    
+    button.removeEventListener('click', addBookToLibrary);
+}
+
+function addForm() {
+    const form = document.createElement('form');
+    for (let i = 0; i < 3; i++) {
+        const input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        form.appendChild(input);        
+    }
+    container.appendChild(form);
+    button.removeEventListener('click', addForm);
 }
 
 const displayBook = () => {
     for (let i = 0; i < myLibrary.length; i++) {
         const p = document.createElement('p');
         p.textContent = myLibrary[i];
-        console.log(p)
-        document.getElementById('container').appendChild(p)
+        console.log(p);
+        container.appendChild(p);
     }
     
 }
 
-displayBook();
+const container = document.getElementById('container');
+const button = document.querySelector("#container > button");
+
+button.addEventListener('click', addForm);
