9dbe5675fce02ccc35c22c48c97ce74026d98c08
mremoto <mremotoo@gmail.com>
2022-09-01 23:44:42 +0800

    Very basic functionality done. Add todo comments.

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ let myLibrary = [];
 //constructor
 function Book() {}
 
-function addBookToLibrary(e) {
-  
-  console.log(document.querySelector("#title").value);
+function addBookToLibrary() {
+  const input = document.getElementsByClassName("book-input");
+  for (let i = 0; i < input.length; i++) {
+    myLibrary.push(input.item(i).value);
+  }
+  displayBook();
 }
 
 function formCheck() {
@@ -12,33 +15,47 @@ function formCheck() {
 }
 
 function addForm(e) {
-  buttonBringForm.style.display = "none";
+  btnBringForm.style.display = "none";
   form.style.display = "block";
-  submitForm.style.display = "block";
+  btnsubmitForm.style.display = "block";
 }
 
-function hideElementsUponLoad() {
+function hideElementsOnLoad() {
   form.style.display = "none";
-  submitForm.style.display = "none";
+  btnsubmitForm.style.display = "none";
+}
+
+function clearLibrary() {
+  myLibrary = [];
 }
 
 const displayBook = () => {
-  // for (let i = 0; i < myLibrary.length; i++) {
-  //     const p = document.createElement('p');
-  //     p.textContent = myLibrary[i];
-  //     console.log(p);
-  //     container.appendChild(p);
-  // }
+  const p = document.createElement("p");
+  p.textContent = myLibrary;
+  console.log(p);
+  display.appendChild(p);
+  clearLibrary();
 };
 
-//element vars
+//elements
 const container = document.getElementById("container");
-const buttonBringForm = document.querySelector("#bring-form");
+const btnBringForm = document.querySelector("#bring-form");
 const form = document.querySelector("#container > form");
-const submitForm = document.getElementById("submit-form");
+const btnsubmitForm = document.getElementById("submit-form");
+const display = document.getElementById("display-container");
 
-//event listeners
-buttonBringForm.addEventListener("click", addForm);
-submitForm.addEventListener("click", addBookToLibrary)
+//listeners
+btnBringForm.addEventListener("click", addForm);
+btnsubmitForm.addEventListener("click", addBookToLibrary);
 
-hideElementsUponLoad();
+hideElementsOnLoad();
+
+// Todo:
+// fix display to display values from array separately
+
+// Pending as per project:
+// Add a button on each book’s display to remove the book from the library.
+// You will need to associate your DOM elements with the actual book objects in some way. One easy solution is  giving them a data-attribute that corresponds to the index of the library array.
+// Add a button on each book’s display to change its read status.
+// To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
+// NOTE: You’re not required to add any type of storage right now. You will have the option to come back to this project later on in the course.
